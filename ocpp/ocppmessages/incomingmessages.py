4631f0474423a48284880a4bf94624ad1fab4caa
ugurdin <ugurdincer1998@gmail.com>
2023-02-20 09:34:24 +0100

    missing dataclasses were added.

--- ocpp/ocppmessages/incomingmessages.py
+++ ocpp/ocppmessages/incomingmessages.py
@@ -1,7 +1,309 @@
 from dataclasses import dataclass, field
+from typing import Dict, List, Optional
 
 @dataclass
 class BootNotificationResponsePayload:
     currentTime: str
     interval: int
     status: str = field(default = "Accepted")
+
+@dataclass 
+class HeartBeatResponsePayload:
+    currentTime: str
+
+@dataclass
+class AuthorizeResponsePayload:
+    idTokenInfo:Dict
+
+@dataclass
+class CancelReservationRequestPayload:
+    reservationId: int
+
+@dataclass
+class CertificateSignedRequestPayload:
+    certificateChain: str
+
+@dataclass
+class ChangeAvailabilityRequestPayload:
+    operationalStatus: str
+
+@dataclass
+class ClearCacheRequestPayload:
+    pass
+
+@dataclass
+class ClearChargingProfileRequestPayload:
+    pass
+
+@dataclass
+class ClearDisplayMessageRequestPayload:
+    id: int
+
+@dataclass
+class ClearedChargingLimitResponsePayload:
+    pass
+
+@dataclass
+class ClearVariableMonitoringRequestPayload:
+    id: int
+
+@dataclass
+class CostUpdatedRequestPayload:
+    totalCost: int
+    transactionId: str
+
+@dataclass
+class CustomerInformationRequestPayload:
+    requestId: int
+    report: bool
+    clear: bool
+
+@dataclass
+class DataTransferRequestPayload:
+    vendorId: str
+
+@dataclass
+class DeleteCertificateRequestPayload:
+    certificateHashData: Dict
+
+@dataclass
+class FirmwareStatusNotificationResponsePayload:
+    pass
+
+@dataclass
+class Get15118EVCertificateResponsePayload:
+    status: str
+    exiResponse: str
+
+@dataclass
+class GetBaseReportRequestPayload:
+    requestId: int
+    reportBase: str
+
+@dataclass
+class GetCertificateStatusResponsePayload:
+    status: str
+
+@dataclass
+class GetChargingProfilesRequestPayload:
+    requestId: int
+    chargingProfile: Dict
+
+@dataclass
+class GetCompositeScheduleRequestPayload:
+    duration: int
+    evseId: int
+
+@dataclass
+class GetDisplayMessagesRequestPayload:
+    requestId: int
+
+@dataclass
+class GetInstalledCertificateIdsRequestPayload:
+    pass
+
+@dataclass
+class GetLocalListVersionRequestPayload:
+    pass
+
+@dataclass
+class GetLogRequestPayload:
+    logType:  str
+    requestId: int
+    log: Dict
+  
+@dataclass
+class GetMonitoringReportRequestPayload:
+    requestId: int
+
+@dataclass
+class GetReportRequestPayload:
+    requestId: int
+
+@dataclass
+class GetTransactionStatusRequestPayload:
+    pass
+
+@dataclass
+class GetVariablesRequestPayload:
+    getVariableData: Dict
+
+@dataclass
+class InstallCertificateRequestPayload:
+    certificate: str
+    certificateType: str
+    
+@dataclass
+class LogStatusNotificationResponsePayload:
+    pass
+
+@dataclass
+class MeterValuesResponsePayload:
+    pass
+
+@dataclass
+class NotifyChargingLimitResponsePayload:
+    pass
+
+@dataclass
+class NotifyCustomerInformationResponsePayload:
+    pass
+
+@dataclass
+class NotifyDisplayMessagesResponsePayload:
+    pass
+
+@dataclass
+class NotifyEVChargingNeedsResponsePayload:
+    status: str
+
+@dataclass
+class NotifyEVChargingScheduleResponsePayload:
+    status: str
+
+@dataclass
+class NotifyEventResponsePayload:
+    pass
+
+@dataclass
+class NotifyMonitoringReportResponsePayload:
+    pass
+
+@dataclass
+class NotifyReportRequestPayload:
+    pass
+
+@dataclass
+class PublishFirmwareRequestPayload:
+    location: str
+    checksum: str
+    requestId: int
+
+@dataclass
+class PublishFirmwareStatusNotificationResponsePayload:
+    pass
+
+@dataclass
+class ReportChargingProfilesResponsePayload:
+    pass
+
+@dataclass
+class RequestStartTransactionRequestPayload:
+    remoteStartId: int
+    idToken: Dict
+
+@dataclass
+class RequestStopTransactionRequestPayload:
+    transactionId: str
+
+@dataclass
+class ReservationStatusUpdateResponsePayload:
+    pass
+
+@dataclass
+class ReserveNowRequestPayload:
+    id: int
+    expiryDateTime: str
+    idToken: Dict
+
+@dataclass
+class ResetRequestPayload:
+    type: str
+
+@dataclass
+class SecurityEventNotificationResponsePayload:
+    pass
+
+@dataclass
+class SendLocalListRequestPayload:
+    versionNumber: int
+    updateType: str
+
+@dataclass
+class SetChargingProfileRequestPayload:
+    evseId: int
+    chargingProfile: Dict
+
+@dataclass
+class SetDisplayMessageRequestPayload:
+    message: Dict
+
+@dataclass
+class SetMonitoringBaseRequestPayload:
+    monitoringBase: str
+
+@dataclass
+class SetMonitoringLevelRequestPayload:
+    severity: int
+
+@dataclass
+class SetNetworkProfileRequestPayload:
+    configurationSlot:int
+    connectionData: Dict
+
+@dataclass
+class SetVariableMonitoringRequestPayload:
+    setMonitoringData: Dict
+
+@dataclass
+class SetVariablesRequestPayload:
+    setVariableData: Dict
+
+@dataclass
+class SignCertificateResponsePayload:
+    status: str
+
+@dataclass
+class StatusNotificationResponsePayload:
+    pass
+
+@dataclass
+class TransactionEventResponsePayload:
+    pass
+
+@dataclass
+class TriggerMessageRequestPayload:
+    requestedMessage: str
+
+@dataclass
+class UnlockConnectorRequestPayload:
+    evseId: int
+    connectorId: int
+
+@dataclass
+class UnpublishFirmwareRequestPayload:
+    checksum: str
+
+@dataclass 
+class UpdateFirmwareRequestPayload:
+    requestId: int
+    firmware: Dict
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+        
+
+    
+
+
+
+
--- ocpp/ocppmessages/outgoingmessages.py
+++ ocpp/ocppmessages/outgoingmessages.py
@@ -4,9 +4,317 @@ from typing import Dict, List, Optional
 
 @dataclass
 class BootNotificationRequestPayload:
-    charging_station: Dict
+    chargingStation: Dict
     reason: str = field(default = "PowerUp")
 
 @dataclass
 class HeartbeatRequestPayload:
     pass
+
+@dataclass
+class AuthorizeRequestPayload:
+    idtoken: Dict
+
+@dataclass
+class CancelReservationResponsePayload:
+    status: str
+
+@dataclass
+class CertificateSignedResponsePayload:
+    status: str
+
+@dataclass
+class ChangeAvailabilityResponsePayload:
+    status: str
+
+@dataclass
+class ClearCacheResponsePayload:
+    status: str
+
+@dataclass
+class ClearChargingProfileResponsePayload:
+    status: str
+
+@dataclass
+class ClearDisplayMessageResponsePayload:
+    status: str
+
+@dataclass
+class ClearedChargingLimitRequestPayload:
+    chargingLimitSource: str
+
+@dataclass
+class ClearVariableMonitoringResponsePayload:
+    clearMonitoringResult: Dict
+
+@dataclass
+class CostUpdatedResponsePayload:       
+    pass
+
+@dataclass
+class CustomerInformationResponsePayload:
+    status: str
+
+@dataclass
+class DataTransferResponsePayload: 
+    status: str   
+
+@dataclass
+class DeleteCertificateResponsePayload:
+    status: str
+
+@dataclass
+class FirmwareStatusNotificationRequestPayload:
+    status: str
+
+@dataclass
+class Get15118EVCertificateRequestPayload:
+    iso15118SchemaVersion: str
+    action: str
+    exiRequest: str
+
+@dataclass
+class GetBaseReportResponsePayload:
+    status: str
+
+@dataclass
+class GetCertificateStatusRequestPayload:
+    ocspRequestData: Dict
+
+@dataclass
+class GetChargingProfilesResponsePayload:
+    status: str
+
+@dataclass
+class GetCompositeScheduleResponsePayload:
+    status: str
+
+@dataclass
+class GetDisplayMessagesResponsePayload:
+    status: str
+
+@dataclass
+class GetInstalledCertificateIdsResponsePayload:
+    status: str
+
+@dataclass
+class GetLocalListVersionResponsePayload:
+    versionNumber: int
+
+@dataclass
+class GetLogResponsePayload:
+    status: str
+
+@dataclass
+class GetMonitoringReportResponsePayload:
+    status: str
+
+@dataclass
+class GetReportResponsePayload:
+    status: str
+
+@dataclass
+class GetTransactionStatusResponsePayload:
+    messagesInQueue: bool
+
+@dataclass
+class GetVariablesResponsePayload:
+    getVariableResult: Dict
+
+@dataclass
+class InstallCertificateResponsePayload:
+    status: str
+
+@dataclass
+class LogStatusNotificationRequestPayload:
+    status: str
+
+@dataclass
+class MeterValuesRequestPayload:
+    evseId: int
+    meterValue: Dict
+
+@dataclass
+class NotifyChargingLimitRequestPayload:
+    chargingLimit: Dict
+
+@dataclass
+class NotifyCustomerInformationRequestPayload:
+    data: str
+    seqNo: int
+    generatedAt: str
+    requestId: int
+
+@dataclass
+class NotifyDisplayMessagesRequestPayload:
+    requestId: int
+
+@dataclass
+class NotifyEVChargingNeedsRequestPayload:
+    evseId: int
+    chargingNeeds: Dict
+
+@dataclass
+class NotifyEVChargingScheduleRequestPayload:
+    timeBase: str
+    evseId: int
+    chargingSchedule: Dict
+
+@dataclass
+class NotifyEventRequestPayload:
+    generatedAt: str
+    seqNo: int
+    eventData: Dict
+
+@dataclass
+class NotifyMonitoringReportRequestPayload:
+    requestId: int
+    seqNo: int
+    generatedAt: str
+
+@dataclass
+class NotifyReportRequestPayload:
+    requestId: int
+    generatedAt: str
+    seqNo: int
+
+@dataclass
+class PublishFirmwareResponsePayload:
+    status: str
+
+@dataclass
+class PublishFirmwareStatusNotificationRequestPayload:
+    status: str
+
+@dataclass
+class ReportChargingProfilesRequestPayload:
+    requestId: int
+    chargingLimitSource: str
+    evseId: int
+    chargingProfile: Dict
+
+@dataclass 
+class RequestStartTransactionResponsePayload:
+    status: str
+
+@dataclass
+class RequestStopTransactionResponsePayload:
+    status: str
+
+@dataclass
+class ReservationStatusUpdateRequestPayload:
+    reservationId: int
+    reservationUpdateStatus: str
+
+@dataclass
+class ReserveNowResponsePayload:
+    status: str
+
+@dataclass
+class ResetResponsePayload:
+    status: str
+
+@dataclass
+class SecurityEventNotificationRequestPayload:
+    type: str
+    timestamp: str
+
+@dataclass
+class SendLocalListResponsePayload:
+    status: str
+
+@dataclass
+class SetChargingProfileResponsePayload:
+    status: str
+
+@dataclass
+class SetDisplayMessageResponsePayload:
+    status: str
+
+@dataclass
+class SetMonitoringBaseResponsePayload:
+    status: str
+
+@dataclass
+class SetMonitoringLevelResponsePayload:
+    status: str
+
+@dataclass
+class SetNetworkProfileResponsePayload:
+    status: str
+
+@dataclass
+class SetVariableMonitoringResponsePayload:
+    setMonitoringResult: Dict
+
+@dataclass
+class SetVariablesResponsePayload:
+    setVariableResult: Dict
+
+@dataclass
+class SignCertificateRequestPayload:
+    csr: str
+
+@dataclass
+class StatusNotificationRequestPayload:
+    timestamp: str
+    connectorStatus: str
+    evseId: int
+    connectorId: int
+
+@dataclass
+class TransactionEventRequestPayload:
+    eventType: str
+    timestamp: str
+    triggerReason: str
+    seqNo: int
+    transactionInfo: Dict
+
+@dataclass
+class TriggerMessageResponsePayload:
+    status: str
+
+@dataclass
+class UnlockConnectorResponsePayload:
+    status: str
+
+@dataclass
+class UnpublishFirmwareResponsePayload:
+    status: str
+    
+@dataclass
+class UpdateFirmwareResponsePayload:
+    status: str
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
